ba9169a0e63ff4a0500e1b3ae43f1eacfbd34966
Caitlin Mac <caitlin.maceachen@gmail.com>
2024-06-06 13:08:18 -0400

    Static components of streamlit page completed

---
+++ heartbd-app.py
@@ -0,0 +1,28 @@
+import streamlit as st
+import requests
+
+'''
+# Heart Beat Decoder
+
+## A machine learning model to classify ECG results
+'''
+
+st.markdown('''
+## First, please record or upload your ecg results and indicate their format:
+''')
+
+# input_columns = st.columns(2)
+st.button("Upload ECG results")
+st.button("Analyze my results")
+
+
+# Call API to run model on input data
+url = 'https://taxifare.lewagon.ai/predict'
+params = {}
+response = requests.get(url, params=params)
+
+# Output results
+st.markdown('''
+## The results are in...
+''')
+st.write(f"{response.json()}")
